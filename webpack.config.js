3d5a5d4ff4d4897958b0bf3839f1e91f97f3a922
guesswhozzz <torridtime@gmail.com>
2020-12-17 16:39:34 +0300

    Add basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: { index: './index.js' },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new HTMLWebpackPlugin({
+      template: '../index.html',
+      chunks: ['preloader', 'index'],
+    }),
+    new CleanWebpackPlugin({
+      verbose: true,
+      dry: false,
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'src', 'public'), to: 'public' },
+      ],
+    }),
+  ],
+};
