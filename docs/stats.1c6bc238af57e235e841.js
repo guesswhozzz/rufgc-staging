a619a94511c988348a24bbe8e00b66244a2c5a1f
guesswhozzz <torridtime@gmail.com>
2020-12-22 13:37:01 +0300

    Initial commit

---
+++ docs/stats.1c6bc238af57e235e841.js
@@ -0,0 +1,15 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!******************!*
+  !*** ./stats.js ***!
+  \******************/
+eval("// YA\n(function (m, e, t, r, i, k, a) {\n  m[i] =\n    m[i] ||\n    function () {\n      (m[i].a = m[i].a || []).push(arguments);\n    };\n  m[i].l = 1 * new Date();\n  (k = e.createElement(t)),\n    (a = e.getElementsByTagName(t)[0]),\n    (k.async = 1),\n    (k.src = r),\n    a.parentNode.insertBefore(k, a);\n})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');\nym(70704454, 'init', {\n  clickmap: true,\n  trackLinks: true,\n  accurateTrackBounce: true,\n});\n// GOOGLE\nwindow.dataLayer = window.dataLayer || [];\nfunction gtag() {\n  dataLayer.push(arguments);\n}\ngtag('js', new Date());\ngtag('config', 'UA-155103751-1');\n\n\n//# sourceURL=webpack:///./stats.js?");
+/******/ })()
+;
