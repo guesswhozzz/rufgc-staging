e5d98bf27aea1f54374381c50c3b1b30e3ba6d98
guesswhozzz <torridtime@gmail.com>
2021-12-31 19:22:02 +0300

    Build pre 1.1.2 ver

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     publicPath: '',
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: [path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new MiniCssExtractPlugin({
